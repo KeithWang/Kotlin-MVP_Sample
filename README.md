353cf232df2ec7058adbc0ea5809ebe8a71cc352
MinHan <wangminhan.keith@gmail.com>
2019-08-21 17:26:41 +0800

    modify mk file

---
+++ README.md
@@ -0,0 +1,4 @@
+# Android Kotlin MVP
+
+## reference
+[googlesamples/android-architecture](https://github.com/googlesamples/android-architecture)
--- README.mk
+++
@@ -1,4 +0,0 @@
-# Android Kotlin MVP
-
-## reference
-[googlesamples/android-architecture](https://github.com/googlesamples/android-architecture)
